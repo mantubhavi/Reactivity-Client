65198bdbcdb3330ac2a2f0da32fee5902773b6ec
Mahantesh <mantu.bhavi@gmail.com>
2026-02-18 00:29:27 +0530

    creating base axios instance

--- src/lib/hooks/useActivities.ts
+++ src/lib/hooks/useActivities.ts
@@ -1,16 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import type { Activity } from "../types/index.d";
+import agent from "../api/agent";
 
 const useActivities = () => {
-    const { data: activities, isPending } = useQuery({
-        queryKey: ["activities"],
-        queryFn: async () => {
-            const response = await axios.get<Activity[]>("http://localhost:5003/api/activities");
-            return response.data
-        }
-    });
-    return { activities, isPending }
-}
+  const { data: activities, isPending } = useQuery({
+    queryKey: ["activities"],
+    queryFn: async () => {
+      const response = await agent.get<Activity[]>("/activities");
+      return response.data;
+    },
+  });
+  return { activities, isPending };
+};
 
 export default useActivities;
