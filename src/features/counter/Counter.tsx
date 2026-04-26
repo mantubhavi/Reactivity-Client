6c882411bd651f4f4d458cfe71b37c6ca6a8872a
Mahantesh <mantu.bhavi@gmail.com>
2026-04-26 18:08:37 +0530

    computed properties

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,48 +1,67 @@
-import { Button, ButtonGroup, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  List,
+  ListItem,
+  Typography,
+  Paper,
+} from "@mui/material";
 import useStore from "../../lib/hooks/useStore";
 import { observer } from "mobx-react-lite";
 
-const Counter = () => {
+const Counter = observer(function Counter() {
   const { counterStore } = useStore();
   return (
-    <>
-      <Typography variant="h4" gutterBottom>
-        {counterStore.title}
-      </Typography>
+    <Box display="flex" justifyContent="space-between">
+      <Box sx={{ width: "50%" }}>
+        <Typography variant="h4" gutterBottom>
+          {counterStore.title}
+        </Typography>
 
-      <Typography variant="h6" gutterBottom>
-        The count is {counterStore.count}
-      </Typography>
+        <Typography variant="h6" gutterBottom>
+          The count is {counterStore.count}
+        </Typography>
 
-      <ButtonGroup sx={{ mt: 3 }}>
-        <Button
-          variant="contained"
-          color="error"
-          onClick={() => counterStore.decrement()}
-        >
-          Decrement
-        </Button>
+        <ButtonGroup sx={{ mt: 3 }}>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={() => counterStore.decrement()}
+          >
+            Decrement
+          </Button>
 
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => counterStore.increment()}
-        >
-          Increment
-        </Button>
+          <Button
+            variant="contained"
+            color="success"
+            onClick={() => counterStore.increment()}
+          >
+            Increment
+          </Button>
 
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => counterStore.increment(5)}
-        >
-          Increment by 5
-        </Button>
-      </ButtonGroup>
-    </>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => counterStore.increment(5)}
+          >
+            Increment by 5
+          </Button>
+        </ButtonGroup>
+      </Box>
+
+      <Paper sx={{ width: "40%", p: 4 }}>
+        <Typography variant="h5">
+          Counter events ({counterStore.eventCount})
+        </Typography>
+        <List>
+          {counterStore.events.map((e, index) => (
+            <ListItem key={`${e}-${index}`}>{e}</ListItem>
+          ))}
+        </List>
+      </Paper>
+    </Box>
   );
-};
+});
 
-const CounterObserver = observer(Counter);
-
-export default CounterObserver;
+export default Counter;
--- src/lib/stores/counterStore.ts
+++ src/lib/stores/counterStore.ts
@@ -1,23 +1,27 @@
-import { action, makeObservable, observable } from "mobx";
+import { makeAutoObservable } from "mobx";
 
 export default class CounterStore {
   title = "Counter store";
   count = 42;
+  events: string[] = [
+    `Initial count is ${this.count}`
+  ];
 
   constructor() {
-    makeObservable(this, {
-      title: observable,
-      count: observable,
-      increment: action,
-      decrement: action,
-    });
+    makeAutoObservable(this);
   }
 
   increment = (amount = 1) => {
     this.count += amount;
+    this.events.push(`Incremented by ${amount} - count is now ${this.count}`);
   };
 
   decrement = (amount = 1) => {
     this.count -= amount;
+    this.events.push(`Decemented by ${amount} - count is now ${this.count}`);
   };
+
+  get eventCount() {
+    return this.events.length;
+  }
 }
